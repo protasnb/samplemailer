6f43f04c57738dac3cf5c93c90345e2616d02768
Nata Protas <protasnb@yahoo.com>
2018-12-27 13:35:37 -0800

    added comments view partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-    resources :articles   
-	root :to => 'pages#index'
-    get 'pages/contacts'
-	get 'pages/about'
+  resources :articles do
+	resources :comments
   end
+  resources :contacts  
+  root :to => 'pages#index'
+  get 'pages/contacts'
+  get 'pages/about'
+end
